f2b69c75d8ba768eb6efa74f034fb069729eb170
GauravRaj810 <gauravskg810@gmail.com>
2025-05-06 11:25:53 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./componentss/Header";
+import Entry from "./componentss/entry";
+
+import { Fragment } from "react";
+
+export default function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Entry />
+    </Fragment>
+  );
+}
---
+++ src/componentss/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="bg-[#F55A5A] py-4">
+      <div className="container flex justify-center gap-3 items-center">
+        <div className="logo-img max-w-8">
+        <img src="/Globe.png" />
+        </div>
+        <h2  className="text-white text-lg font-medium">My Travel Journal</h2  >
+       </div>
+    </header>
+  );
+}
